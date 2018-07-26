4e7bd379c41add9ae428153300c4860447e47255
xXInfinityMastered <41016349+xXInfinityMastered@users.noreply.github.com>
2018-07-26 16:56:41 -0500

    Add files via upload

--- Read_Me.txt
+++ Read_Me.txt
@@ -1,5 +1,20 @@
-Park Name: Wesley + Micheal
-Events: Wesley
-Jose: About
-Contact info stuff: Micheal
-Rides: William
+Index:Wesley, William, Michael(mostly), Jose
+Events: Wesley(all event pages) 
+About: Jose
+Contact info stuff: Michael
+Rides: William, Wesley
+CSS:anyone who needs to use it
+Javascript:The work is here'nt
+Tickets:William
+Map: Michael
+images:Wesley, William
+
+
+Wesley: Events(and all event pages) images, rides main mage, rules page
+Michael: Contact Info, Index, About
+About: Jose
+William: Extreme and easy Rides pages & Tickets, images(some)
+
+
+class="mx-auto" style="width:400px" (will delete soon)          2:12pm: Please delete this soon ~ Wesley 
+
--- javascript.js
+++ javascript.js
@@ -1,19 +1,50 @@
-//7b4c123850177a48dc2ee0b6299083c6
+//b6f0cb12d38fb1510fc6fb510c01a8da
 $(document).ready(function () {
     $(".submit").click(function () {
         //Make the API Call
-            var city = $(".city").val();
-            var apiKey = "7b4c123850177a48dc2ee0b6299083c6";
+            var city = 33629;
+            var apiKey = "b6f0cb12d38fb1510fc6fb510c01a8da";
             var uri = "https://api.openweathermap.org/data/2.5/weather?zip=" + city + ",us&units=imperial&appid=" + apiKey;
             console.log(uri);
             $.get(uri, onRes);
     });
 });
 
-function onRes(response) { //"response is actually a variable, we could name it whatever we want. It just stores what the api sends."
-    console.log(response);
-    $(".country").html(response.sys.country);
-    $(".temp").html(response.main.temp);
-    $(".city-name").html(response.name);
-    $(".weather").html(response.weather[0].description)
+function onRes(response) {
+//reponse is a variable
+console.log(response);
+$(".country").html(response.sys.country);
+$(".temp").html(response.main.temp);
+$(".city-name").html(response.name);
+$(".weather").html(response.weather[0].description);
+}
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
